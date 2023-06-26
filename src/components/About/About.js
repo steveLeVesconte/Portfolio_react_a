d95bf8182ab053fc19711904f4aa4982236b5253
steveLeVesconte <SteveLeVesconte@Gmail.com>
2023-06-26 09:44:01 -0700

    Remove unused references

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Particle from "../Particle";
-import Github from "./Github";
+/* import Particle from "../Particle";
+import Github from "./Github"; */
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+/* import laptopImg from "../../Assets/about.png"; */
 import laptopAngle from "../../Assets/laptopAngle.png";
 import Toolstack from "./Toolstack";
 
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
+/* import { CgCPlusPlus } from "react-icons/cg"; */
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
+/*   DiMongodb,
   DiPython,
   DiGit,
-  DiJava,
+  DiJava, */
   DiAngularSimple,
   DiCss3,
 } from "react-icons/di";
 import {
-  SiRedis,
+/*   SiRedis,
   SiFirebase,
   SiNextdotjs,
   SiSolidity,
-  SiPostgresql,
+  SiPostgresql, */
   SiDotnet,
   SiMicrosoftsqlserver,
   SiMicrosoftazure,
   SiCsharp,
   SiHtml5,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+/* import { TbBrandGolang } from "react-icons/tb"; */
 
 function Techstack() {
   return (
